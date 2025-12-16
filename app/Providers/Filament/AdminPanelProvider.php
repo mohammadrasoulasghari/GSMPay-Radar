02c0e93fd4889fd09bc857e88174b15a5d527fed
mohammadrasoul asghari <mohammadrasoulasghari1@gmail.com>
2025-12-16 10:27:37 +0330

    feat(i18n): set application and Filament locale to Persian (fa)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('GSMPay Radar')
             ->pages([
                 Dashboard::class,
             ])
